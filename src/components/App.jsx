ce80190acf8439b2364285b4a134bc8caf3ec2ab
Presy Lord Kidlat <91861180+presylord@users.noreply.github.com>
2021-12-05 00:44:42 +0800

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [first, setFirst] = useState("");
+  function firstName(event) {
+    setFirst(event.target.value);
+  }
+
+  const [last, setLast] = useState("");
+  function lastName(event) {
+    setLast(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {first} {last}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input name="fName" placeholder="First Name" onChange={firstName} />
+        <input name="lName" placeholder="Last Name" onChange={lastName} />
         <button>Submit</button>
       </form>
     </div>
